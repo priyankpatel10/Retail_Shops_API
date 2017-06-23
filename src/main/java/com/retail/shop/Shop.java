777dbda0103611f1e39cdcdf838cff1dcdbc287e
priyank <priyank@Priyank>
2017-06-23 16:13:29 +0530

    Initial Commit

---
+++ src/main/java/com/retail/shop/Shop.java
@@ -0,0 +1,62 @@
+package com.retail.shop;
+
+public class Shop {
+	private String shopName;
+	private String shopAddress;
+	private String shopPostCode;
+	private String latitude;
+	private String longitude;
+	
+	public Shop(String shopName, String shopAddress, String shopPostCode, String latitude, String longitude) {
+		super();
+		this.shopName = shopName;
+		this.shopAddress = shopAddress;
+		this.shopPostCode = shopPostCode;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+	public String getShopAddress() {
+		return shopAddress;
+	}
+
+	public void setShopAddress(String shopAddress) {
+		this.shopAddress = shopAddress;
+	}
+
+	public String getShopPostCode() {
+		return shopPostCode;
+	}
+
+	public void setShopPostCode(String shopPostCode) {
+		this.shopPostCode = shopPostCode;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/retail/shop/ShopMain.java
@@ -0,0 +1,16 @@
+package com.retail.shop;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMain.class, args);
+    }
+
+}
+
